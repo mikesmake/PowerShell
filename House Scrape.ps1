49adfe7aa69ffe5b77a2cc8a5fc5672bde7669e9
mikesmake <51163865+mikesmake@users.noreply.github.com>
2024-02-01 12:58:59 +0000

    Add files via upload

--- House Scrape.ps1
+++ House Scrape.ps1
@@ -1,40 +1,48 @@
-$links = (Invoke-WebRequest "https://www.persimmonhomes.com/new-homes/yorkshire/germany-beck").links | Where-Object { $_.innerText -eq "View more information" }
-
-
+# create empty vars
 $plotnumber = @()
 $cost = @()
 $output = @()
+$out = @()
 
+# get all available house types
+$links = (Invoke-WebRequest "https://www.persimmonhomes.com/new-homes/yorkshire/germany-beck").links | Where-Object { $_.innerText -eq "View more information" }
+
+# for each house type get plots 
 foreach ($link in $links) {
-    $href = ($link.href | Out-String)
-    $name = $href.split('/')[4]
-    $r = (Invoke-WebRequest "https://www.persimmonhomes.com$href").content  
+    $href = ($link.href | Out-String) # pulls house link
+    $house = $href.split('/')[4]                # 
+    $TextInfo = (Get-Culture).TextInfo          # gets house name from url & cleans
+    $houseclean = $house -replace '-', ' '      #
+    $name = $TextInfo.ToTitleCase($houseclean)  #
+    $request = (Invoke-WebRequest "https://www.persimmonhomes.com$href").content # gets house info
+    $lines = $request.Split([Environment]::NewLine) # makes content searchable 
+    $plots = $lines | Select-String -Pattern "<div class=`"table-mobile-layout__cell-body`">" -Context 1 -AllMatches # pull back all plot number and prices
+    $i = 1 # used to create object only after each plot and price have been looped
 
-    $lines = $r.Split([Environment]::NewLine)
-
-    $plots = $lines | Select-String -Pattern "<div class=`"table-mobile-layout__cell-body`">" -Context 1 -AllMatches
-       
-
+    # get plot numbers and prices for each plot
     foreach ($plot in $plots) {
-        $housename = $name
-
+        $i++ # every other loop creates output with both plot and price
+        # get plot number and tidy
         if ($plot -like "*</div>*") {
-
             $plotnumber = ($plot.line.Split('>')[1]).trimend("</div") 
         }
-
+        # get price and tidy
         else {
-            $cost = ($plot.line.Split('>')[1].trim()).trimstart("&#163;")
+            $cost = ($plot.line.Split('>')[1].trim()) -replace '&#163;', ''  # .trimstart("&#163;")
+            if ($cost -eq "Reserved") { $cost = 0 }
         }
-
-        $out = New-Object psobject -Property @{
-            "Name" = $name
-            "Plot" = $plotnumber
-            "Cost" = $cost
+        # every other loop creates output with both plot and price
+        if ([int]($i) % 2 -eq 1) {
+            $out = New-Object psobject -Property @{
+                "Name" = $name
+                "Plot" = $plotnumber
+                "Cost" = $cost
+                "Date" = Get-Date
+            }
+            $output += $out
         }
-
-        $output += $out
-
     }
-
 }
+
+$output
+
